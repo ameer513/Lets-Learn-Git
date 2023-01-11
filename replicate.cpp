39822110f8a9ee4d40823263a80cec1efc4f1e8a
ameer513 <ameerameer513@gmail.com>
2023-01-12 04:45:54 +0530

    replicate added

---
+++ replicate.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+void bubblesort()
+{
+    // 
+}
+void quicksort()
+{
+    // 
+}
+void margesort()
+{
+    // 
+}
+void insertionsort()
+{
+    // 
+}
+int main()
+{
+    std::cout<<"Hello world";
+    std::cout<<"Ameer Hamza";
+}
